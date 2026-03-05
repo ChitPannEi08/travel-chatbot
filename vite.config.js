e7f21d8fdaa2b621e05b1f7088abcfaa743c60fc
ChitPannEi08 <chitpannei08@gmail.com>
2026-03-05 22:38:51 +0700

    Configure Vite and package.json for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/thailand-travel-chatbot-agent/",
   plugins: [react()],
+  base: '/website_test/',
 })
