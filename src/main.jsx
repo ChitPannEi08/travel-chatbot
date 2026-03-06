8610b9acdb17e7343db1fd3ba468963d7e5babcc
ChitPannEi08 <chitpannei08@gmail.com>
2026-03-06 13:00:37 +0700

    Update hosting configuration for new repository

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/website_test">
+    <BrowserRouter basename="/travel-chatbot">
       <App />
     </BrowserRouter>
   </StrictMode>,
