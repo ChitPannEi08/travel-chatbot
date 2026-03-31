5fb2e6b54bf14550807028fe7e0e3814889c7616
ChitPannEi08 <chitpannei08@gmail.com>
2026-03-31 13:32:52 +0700

    Initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,11 +7,11 @@ import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import WeatherDashboard from '../components/WeatherDashboard';
 import { useLanguage } from "../context/LanguageContext";
-import image1 from "../assets/image1.jpeg";
-import image2 from "../assets/image2.jpeg";
-import image3 from "../assets/image3.jpeg";
-import image4 from "../assets/image4.jpeg";
-import image5 from "../assets/image5.jpeg";
+import image1 from "../assets/bangkok.jpeg";
+import image2 from "../assets/chiangrai.jpeg";
+import image3 from "../assets/chiangmai.jpeg";
+import image4 from "../assets/pattaya.jpeg";
+import image5 from "../assets/phuket.jpeg";
 
 const Slideshow = () => {
     const images = [
